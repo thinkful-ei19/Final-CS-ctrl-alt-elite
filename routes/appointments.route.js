fd821eadc3119652b19420594f659a349c2dcd87
Julie K <julieskim160@gmail.com>
2018-06-13 11:57:24 -0700

    appointment skeleton

--- routes/appointments.route.js
+++ routes/appointments.route.js
@@ -9,8 +9,9 @@ const nodemailer = require('nodemailer');
 const moment = require('moment');
 const { dbConnect } = require('./db-mongoose');
 const agenda = require('agenda');
-// const agenda = new Agenda ({db: {address:dbConnect}})
 
+
+const agenda = new Agenda ({db: {address:dbConnect}})
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -19,34 +20,36 @@ let transporter = nodemailer.createTransport({
         pass: process.env.GMAIL_PASS
       }
 })
+
 const appointmentTime = moment(newApt.time).format('MMMM Do YYYY, h:mm:ss a')
 
 
+agenda.define('send appointment reminder', {priority: 'high'}, function(job, done) {
+    
+    Appointment.find()
+      .then(()=> {
 
-// agenda.define('send appointment reminder', {priority: 'high'}, function(job, done) {
-//     const appt = {time}
-//     Appointment.find()
+      })
 
-//     let mailOptions = {
-//         from: 'CTRL ALT ELITE <ctrl.alt.elite.acjj@gmail.com>',
-//         to: `${appt.client.email}`,  
-//         subject: `REMINDER: Your ${appt.time} Appointment with CTRL ALT ELITE`,
-//         html: `<p>Hi ${appt.client.name}, <br/> This is a friendly reminder for your appointment
-//         with CTRL ALT ELITE is at ${appt.time}. <br/>Looking forward to seeing you soon! <br/><br/>If you need to schedule, please contact us at PHONE NUMBER. </p>`
-//     };
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         console.log('Message sent', info.messageId);
-//     });    
-//     //.processEvery('24 hours')
+    let mailOptions = {
+        from: 'CTRL ALT ELITE <ctrl.alt.elite.acjj@gmail.com>',
+        to: `${appt.client.email}`,  
+        subject: `REMINDER: Your ${appt.time} Appointment with CTRL ALT ELITE`,
+        html: `<p>Hi ${appt.client.name}, <br/> This is a friendly reminder for your appointment
+        with CTRL ALT ELITE is at ${appt.time}. <br/>Looking forward to seeing you soon! <br/><br/>If you need to schedule, please contact us at PHONE NUMBER. </p>`
+    };
+    transporter.sendMail(mailOptions, function (error, response) {
+        console.log('Message sent: ' + response.message);
+        transporter.close();
+        done();
+    });
 
-// })
+})
 
-// agenda.on('24 hours before', function() {
-//     agenda.every
-// })
+agenda.on('24 hours before', function() {
+    agenda.schedule('24 hours before', )
+    agenda.start();
+})
 
 
 //Create a new appointment.
@@ -107,60 +110,63 @@ router.post('/appointments/:id', (req, res, next) => {
 
 
 router.delete('/appointments/:id', (req, res, next) => {
-  const { id } = req.params;
-  const { userId } = req.body;
-
-  let updateUser;
-  User.findById(userId)
-    .then((result) => {
-      let appointments = [];
-      updateUser = result;
-      updateUser.appointments.forEach((aptId) => {
-        if (String(aptId) !== id) {
-          appointments.push(aptId);
-        }                
-      });
-      updateUser.appointments = appointments;
-      return updateUser;
-    })
-    .then((result) => {
-      User.findByIdAndUpdate(userId, updateUser);
-      return;
-    })
-    .catch(err => next(err));
-
-  Appointment.findByIdAndRemove(id)
-    .then((result) => {
-      res.status(204).end();
-    })
-    .catch((err) => {
-      next(err);
-    });
-
-    // let transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: process.env.GMAIL_USER,
-    //         pass: process.env.GMAIL_PASS
-    //       }
-    // })
+    const { id } = req.params;
+    const { userId } = req.body;
+  
+    let deletedApt;
+    let updateUser;
+  
+  
+    User.findById(userId)
+      .populate('appointments')
+      .populate('clients')
+      .then((result) => {
+        console.log('!!!!', result);
+        let appointments = [];
+        updateUser = result;
+        updateUser.appointments.forEach((aptId) => {
+          if (String(aptId) !== id) {
+            appointments.push(aptId);
+          }                
+        });
+        updateUser.appointments = appointments;
+        return updateUser;
+      })
+      .then((result) => {
+        User.findByIdAndUpdate(userId, updateUser);
+        return;
+      })
+      .catch(err => next(err));
+    Appointment.findById(id)
+      .then((result) => {
+        deletedApt = result;
       
-    // const appointmentTime = moment(newApt.time).format('MMMM Do YYYY, h:mm:ss a')
-
-    let mailOptions = {
-        from: 'CTRL ALT ELITE <ctrl.alt.elite.acjj@gmail.com>',
-        to: `${newApt.client.email}`,  
-        subject: `CANCELLATION: Your ${appointmentTime} Appointment with CTRL ALT ELITE`,
-        html: `<p>Hello ${newApt.client.name}, <br/> Your appointment has successfully been cancelled. 
-        <br/>Have a great day.</p>`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
+        let mailOptions = {
+          from: 'CTRL ALT ELITE <ctrl.alt.elite.acjj@gmail.com>',
+          to: `${deletedApt.client.email}`,  
+          subject: `CANCELLATION: Your ${appointmentTime} Appointment with CTRL ALT ELITE`,
+          html: `<p>Hello ${deletedApt.client.name}, <br/> Your appointment has successfully been cancelled. 
+              <br/>Have a great day.</p>`
+        };
+      
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
             return console.log(error);
-        }
-        console.log('Message sent', info.messageId);
-    });
-});
+          }
+          console.log('Message sent', info.messageId);
+        });
+      })
+      .then(() => {
+        Appointment.findByIdAndRemove(id)
+          .then((result) => {
+            res.status(204).end();
+          })
+          .catch((err) => {
+            next(err);
+          });
+      })
+      .catch(err => next(err));
+    
+  });
 
 module.exports = router;

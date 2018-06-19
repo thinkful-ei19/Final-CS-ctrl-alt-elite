bbc9292e5af48f474b0e3c7e8823e6941647dc8a
Julie K <julieskim160@gmail.com>
2018-06-19 08:37:09 -0700

    thank you email

---
+++ cron/thankyou.js
@@ -0,0 +1,55 @@
+const cron = require('node-cron');
+const Appointment = require('../models/appointment');
+const moment = require('moment');
+const nodemailer = require('nodemailer');
+
+function thanks() {
+    cron.schedule('0 9 * * *', function(){
+    console.log('running a task at 9:00 AM every day');
+    
+    let needsNotification = [];
+
+    Appointment.find()
+        .then((result) => {
+            result.forEach((apt) => {         
+                // day after
+                if (moment(apt.time).subtract(2, 'days').calendary()) {
+                    needsNotification.push(apt);
+                }
+            })
+        })
+        .then(() => {
+            console.log(needsNotification)
+            needsNotification.forEach((apt) => {
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: process.env.GMAIL_USER,
+                  pass: process.env.GMAIL_PASS
+                }
+              });
+
+            const appointmentTime = moment(apt.time).format('MMMM Do YYYY, h:mm a');
+
+              let mailOptions = {
+                from: 'CTRL ALT ELITE <ctrl.alt.elite.acjj@gmail.com>',
+                to: `${apt.client.email}`,  
+                subject: `Thank You!`,
+                html: `<p>Hi there, ${apt.client.name}, <br/>We just wanted to say thanks for using our services! </p>`
+              };
+            
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  return console.log(error);
+                }
+                console.log('Message sent', info.messageId);
+              });
+            })
+
+        })
+        .catch(err => console.error(err))
+    
+ });
+}
+
+module.exports = thanks;
